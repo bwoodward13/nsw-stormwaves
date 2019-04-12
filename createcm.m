1db6fa1aa42661f47a301d8cc4930d2e8e9ea133
bwoodward13 <49504666+bwoodward13@users.noreply.github.com>
2019-04-12 16:56:44 +0800

    Add files via upload

---
+++ createcm.m
@@ -0,0 +1,17 @@
+% Create colour map and set range of heights to plot
+
+CMRmap = jet;
+
+contour_interval=1;
+minT = 0;
+maxT = 10;
+contours=minT:contour_interval:maxT;
+
+%clear sCMRmap
+x=1:63/(length(contours)):64; %get number of color levels to match number of contours
+        xl=1:64;
+        for i=1:3
+        sCMRmap(:,i)=spline(xl,CMRmap(:,i),x)';%splinefitintermediate valucs
+        end
+        sCMRmap=abs (sCMRmap/max(max(sCMRmap)));
+        sCMRmap=sCMRmap(3:end,:);
